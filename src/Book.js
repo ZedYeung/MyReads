615da421e52f1fe120f77d13f012c1024c977e1d
Yue Yang <shoominga@gmail.com>
2017-11-19 14:56:59 -0800

    refactor: Use stateless function rather than component in Book

--- src/Book.js
+++ src/Book.js
@@ -7,40 +7,40 @@ const Book = (props) => {
     updateBook: PropTypes.func.isRequired
   }
 
-    const { book, onUpdateBook } = props
-    const { id, shelf, title, authors, imageLinks, previewLink } = book
+  const { book, onUpdateBook } = props
+  const { id, shelf, title, authors, imageLinks, previewLink } = book
 
-    console.log(book)
+  console.log(book)
 
-    return (
-      <div className="book" id={id} key={id}>
-        <div className="book-top">
-          <a href={previewLink}>
-            <div
-              className="book-cover"
-              style={{
-                width: 128, height: 193,
-                backgroundImage: imageLinks ? `url(${imageLinks.thumbnail})`: null }}>
-            </div>
-          </a>
-          <div className="book-shelf-changer">
-            <select value={shelf || "none"} onChange={(event) => {
-              onUpdateBook(book, event.target.value)
-            }}>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+  return (
+    <div className="book" id={id} key={id}>
+      <div className="book-top">
+        <a href={previewLink}>
+          <div
+            className="book-cover"
+            style={{
+              width: 128, height: 193,
+              backgroundImage: imageLinks ? `url(${imageLinks.thumbnail})`: null }}>
           </div>
+        </a>
+        <div className="book-shelf-changer">
+          <select value={shelf || "none"} onChange={(event) => {
+            onUpdateBook(book, event.target.value)
+          }}>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-          <div className="book-title">
-            <a href={previewLink}>{title}</a>
-          </div>
-        <div className="book-authors">{authors}</div>
       </div>
-    )
+        <div className="book-title">
+          <a href={previewLink}>{title}</a>
+        </div>
+      <div className="book-authors">{authors}</div>
+    </div>
+  )
 }
 
 export default Book
