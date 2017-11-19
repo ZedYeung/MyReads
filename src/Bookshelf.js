c4847601628370f95d64df61af0190697d58d7ec
Yue Yang <shoominga@gmail.com>
2017-11-18 16:05:03 -0800

    feat: Add filter by category

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -11,8 +11,37 @@ class Bookshelf extends Component {
     onUpdateBook: PropTypes.func.isRequired
   }
 
+  state = {
+    category: "all"
+  }
+
+  selectCategory(category) {
+    this.setState({category})
+  }
+
   render() {
     const { books, onUpdateBook } = this.props
+    const { category } = this.state
+
+    let categories = new Set()
+
+    for (let b of books) {
+      for (let c of b.categories) {
+        categories.add(c)
+      }
+    }
+
+    categories = [...categories]
+
+    let showingBooks
+
+    if (category !== "all") {
+      showingBooks =  books.filter((book) => (
+        book.categories.indexOf(category) >= 0
+      ))
+    } else {
+      showingBooks = books
+    }
 
     return (
       <div className="list-books">
@@ -26,7 +55,7 @@ class Bookshelf extends Component {
                 <h2 className="bookshelf-title">{shelf}</h2>
                 <div className="bookshelf-books">
                   <ol className="books-grid">
-                    {books.length > 0 && books.filter((book) => (
+                    {showingBooks.length > 0 && showingBooks.filter((book) => (
                       book.shelf === shelf
                     )).map((book) => (
                       <li key={book.id}>
@@ -42,6 +71,16 @@ class Bookshelf extends Component {
               </div>
             ))}
           </div>
+        </div>
+        <div className="category-changer">
+          <select value={category} onChange={(event) => {
+            this.selectCategory(event.target.value)
+          }}>
+            <option value="all">All</option>
+            {categories.map((category) => (
+              <option value={category} key={category}>{category}</option>
+            ))}
+          </select>
         </div>
         <div className="open-search">
           <Link to='/add'>Add a book</Link>
